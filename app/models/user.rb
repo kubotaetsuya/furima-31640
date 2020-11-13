681c6ca260d5d01860ba4b73433a5c2e5f3d07ee
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-11-13 15:57:09 +0900

    deviseのビューの配置＋usersモデルへのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+
+         validates :nickname, presence: true
+         validates :email, presence: true
+         validates :encrypted_password, presence: true
+         validates :last_name, presence: true
+         validates :first_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :first_name_kana, presence: true
+         validates :birthday, presence: true
+
          has_many :items
          has_many :purchase_records
 end
