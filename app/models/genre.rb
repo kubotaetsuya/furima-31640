5a7784ebd88860f8a0387e5034ba28f53a92189b
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-11-17 14:37:04 +0900

    activehash＋activestorageのアソシエーション定義

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,5 @@
-class Genre < ApplicationRecord
+class Genre < ActiveHash::Base
+  include ActiveHash::Associations
+  has_many :items
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_record
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :price, format: {with:/\A[0-9]+\z/}
+  validates :image, presence :true
 end
