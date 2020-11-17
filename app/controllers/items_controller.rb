c0e1031d5a97973d6658d6dc3eefe728b00854d4
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-11-17 10:58:21 +0900

    itemモデル作成＋itemCSS配置＋出品画面への遷移機能＋ログアウト者の出品画面遷移の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  
   def index
+
   end
+
+  def new
+
+  end
+
+  def create
+    # @item = Item.new(item_params)
+  end
+
+  private
+
+
 end
