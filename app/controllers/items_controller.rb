501e0eb81ab983a146b5716f155fdff82ab7f3cc
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-11-19 17:17:55 +0900

    商品の情報編集機能＋他のユーザーの編集ページの遷移制限＋URL打ち込みの直接的遷移の制限の作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show]
+  before_action :move_to_index, only: [:edit]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,12 +21,35 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_burden_id, :shipping_area_id, :days_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_burden_id, :shipping_area_id, :days_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in? && current_user.id == Item.find(params[:id]).user_id
   end
 end
