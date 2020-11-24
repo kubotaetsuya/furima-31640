ccf6cd4b6acb129427fbb09a7e975cf1acbe0c5e
instructor-salvage <72959612+instructor-salvage@users.noreply.github.com>
2020-11-24 21:14:53 +0900

    購入された商品のページに遷移できない＋sold outの表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
   before_action :set_item_id, only: [:index, :create]
-
+  before_action :authenticate_user!
+  before_action :order_move_to_index, only: [:index, :create]
+  before_action :sold_out_item, only: [:index, :create]
+  
   def index
     @order_address = OrderAddress.new
   end
@@ -27,12 +30,19 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    binding.pry
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item[:price],  
       card: params[:token],   # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
+  end
+
+  def order_move_to_index
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
+  end
+
+  def sold_out_item
+    redirect_to root_path if @item.order.present?
   end
 end
